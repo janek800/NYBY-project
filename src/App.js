bc86c4c0b8e9301ca2c461a2f28c06484b2e43ab
Janek800 <jan.s.radzikowski@gmail.com>
2021-03-18 12:19:56 +0100

    connected WeatherInfo component to data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const [ latitude, longitude, geolocationError ] = useGetGeolocation();
 
-  const url = 'https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=54.381674399999994&lon=18.6111'
+  const url = `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${latitude}&lon=${longitude}`
   const [response, fetchError ] = useFetch(url)
 
   const date = moment().format('YYYY-MM-DDTHH');
@@ -17,12 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      {console.log('latitude is: ', latitude)}
-      {console.log('longitude is: ', longitude)}
-      {console.log('geolocationError is: ', geolocationError)}
-      {console.log('response:', currentHour)}
-      {console.log('fetch error:', fetchError)}
-      <WeatherInfo />
+      <WeatherInfo currentHour={currentHour} geolocationError={geolocationError} fetchError={fetchError}/>
     </div>
   );
 }
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const WeatherInfo = () => {
+const WeatherInfo = ({ geolocationError, fetchError, currentHour}) => {
   return (
     <>
-    <div>current weather: weather</div>
-    <div>current temperature: 4 °C</div>
+    {geolocationError 
+      ?
+      <div>{`Your browser doesn't support geolocation`}</div>
+      : 
+      <div>
+        {fetchError
+        ?
+          <div>Could not fetch weather data</div>
+        :
+          <>
+            <div>current weather: {currentHour?.[0].data.next_1_hours.summary.symbol_code.replace('_', ' ')}</div>
+            <div>current temperature: {currentHour?.[0].data.instant.details.air_temperature} °C</div>
+          </>
+        }
+      </div>
+    }
     </>
   )
 }
