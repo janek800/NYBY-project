0aea6dbb5fb2b58df49700dce3c09160bf943f79
Janek800 <jan.s.radzikowski@gmail.com>
2021-03-18 11:23:15 +0100

    installed moment and implemented filtering for current time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment';
 import WeatherInfo from './components/WeatherInfo'
 import useGetGeolocation from "./hooks/useGetGeolocation";
 import useFetch from "./hooks/useFetch";
@@ -11,12 +12,15 @@ function App() {
   const url = 'https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=54.381674399999994&lon=18.6111'
   const [response, fetchError ] = useFetch(url)
 
+  const date = moment().format('YYYY-MM-DDTHH');
+  const currentHour = response?.timeseries.filter((item) => item.time.includes(date))
+
   return (
     <div className="App">
       {console.log('latitude is: ', latitude)}
       {console.log('longitude is: ', longitude)}
       {console.log('geolocationError is: ', geolocationError)}
-      {console.log('response:', response)}
+      {console.log('response:', currentHour)}
       {console.log('fetch error:', fetchError)}
       <WeatherInfo />
     </div>
